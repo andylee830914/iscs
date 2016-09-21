2946664924c79ecec66c6878b5ed460edbd5d915
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-09-21 21:52:37 +0800

    hw1 finish

--- hw1.php
+++ hw1.php
@@ -8,6 +8,7 @@ require_once('latex/LatexTemplate.php');
 $quy="select * from user where idnumber='".$name."'";
 $result=mysql_query($quy);
 $moodleid=mysql_result($result,0,1);
+$hw=$moodleid % 4;
 $test = "";
 if(isset($_GET['t'])) {
 	// Make the LaTeX file and send it through
@@ -18,7 +19,7 @@ if(isset($_GET['t'])) {
 	);
 
 	try {
-		LatexTemplate::download($data, 'latex/hw1.tex', $name.'.pdf');
+		LatexTemplate::download($data, 'latex/hw1/hw1_'.$hw.'.tex', $name.'_HW1.pdf');
 	} catch(Exception $e) {
 		echo $e -> getMessage();
 	}
@@ -114,12 +115,13 @@ if(isset($_GET['t'])) {
               <li>請按底下的「下載檔案」，下載本次的作業題目PDF檔</li>                           
               <li>上傳規定：
                   <ul>
-                  <li>請回 moodle 上傳本次作業</li>
-                  <li>"作業題目"請與"muPad檔"一起放進資料夾，使用zip壓縮</li>                                                                                    
+                  <li>請回 moodle 上傳本次作業。</li>
+                  <li>"作業題目"請與"MuPAD檔"一起放進資料夾，使用zip壓縮。</li>
+                  <li>請把學號姓名寫在MuPAD檔第一行。</li>                                                                                    
                   <li>檔名：<strong>學號_HW1.zip</strong></li>               
                   </ul>
               </li>
-              <li>期限：2016-09</li>
+              <li>期限：2016-09-26 17:00</li>
               <li>作業請自己寫！不會寫可以跟同學、老師或助教討論！</li>               
 
             
