b27a42f0af7d5c7e261ec7f1bc219897211f868a
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-09-21 16:32:04 +0800

    start edit hw

--- hw1.php
+++ hw1.php
@@ -10,15 +10,7 @@ if(isset($_GET['t'])) {
 	// Make the LaTeX file and send it through
 	$test = $_GET['t'];
 	$data = array(
-			'invoiceNumber' => '234567',
-			'invoiceDate' => '12/5/2014',
-			'invoiceBalance' => '$24.60',
-			'customerName' => 'ExampleCorp',
-			'customerAddress' => '123 Example St, Exampleton ACT.',
-			'invoiceItem' => array(
-				array('item' => 'Foo', 'qty' => '2', 'price' => '2.30', 'total' => '4.60'),
-				array('item' => 'Bar', 'qty' => '4', 'price' => '5.00', 'total' => '20.00')			
-			)
+			'idnumber' => $name
 	);
 
 	try {
