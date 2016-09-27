a0ecf28a7dc442fc38c79e37da0e472b1bbf1122
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-09-27 12:42:51 +0800

    move dir

--- latex/MATLABTemplate.php
+++ latex/MATLABTemplate.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Class for handling latex templating
+ * Class for handling numpy2mat
  * 
  * @author Michael Billington < michael.billington@gmail.com >
  */
 class MATLABTemplate {
 	/**
-	 * Generate a PDF file using xelatex and pass it to the user
+	 * Generate a .mat file using python and pass it to the user
 	 */
 	public static function download($python,$username,$moodleid) {
 		// Pre-flight checks
@@ -18,7 +18,7 @@ class MATLABTemplate {
 	
 		// Perform substitution of variables
 	
-		// Run xelatex (Used because of native unicode and TTF font support)
+		// Run python
 		ob_start();
 		$cmd = sprintf("python ".$python." ".$f);
 		chdir(sys_get_temp_dir());
@@ -27,7 +27,7 @@ class MATLABTemplate {
 		// Test here
 		if(!file_exists($mat_f)) {
 			@unlink($f);
-			throw new Exception("Output was not generated and latex returned: $ret.");
+			throw new Exception("Output was not generated and python returned: $ret.");
 		}
 	
 		// Send through output
@@ -40,46 +40,6 @@ class MATLABTemplate {
 		// Final cleanup
 		@unlink($mat_f);
 		@unlink($f);
-	}
-	
-	/**
-	 * Series of substitutions to sanitise text for use in LaTeX.
-	 *
-	 * http://stackoverflow.com/questions/2627135/how-do-i-sanitize-latex-input
-	 * Target document should \usepackage{textcomp}
-	 */
-	public static function escape($text) {
-		// Prepare backslash/newline handling
-		$text = str_replace("\n", "\\\\", $text); // Rescue newlines
-		$text = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '', $text); // Strip all non-printables
-		$text = str_replace("\\\\", "\n", $text); // Re-insert newlines and clear \\
-		$text = str_replace("\\", "\\\\", $text); // Use double-backslash to signal a backslash in the input (escaped in the final step).
-	
-		// Symbols which are used in LaTeX syntax
-		$text = str_replace("{", "\\{", $text);
-		$text = str_replace("}", "\\}", $text);
-		$text = str_replace("$", "\\$", $text);
-		$text = str_replace("&", "\\&", $text);
-		$text = str_replace("#", "\\#", $text);
-		$text = str_replace("^", "\\textasciicircum{}", $text);
-		$text = str_replace("_", "\\_", $text);
-		$text = str_replace("~", "\\textasciitilde{}", $text);
-		$text = str_replace("%", "\\%", $text);
-	
-		// Brackets & pipes
-		$text = str_replace("<", "\\textless{}", $text);
-		$text = str_replace(">", "\\textgreater{}", $text);
-		$text = str_replace("|", "\\textbar{}", $text);
-	
-		// Quotes
-		$text = str_replace("\"", "\\textquotedbl{}", $text);
-		$text = str_replace("'", "\\textquotesingle{}", $text);
-		$text = str_replace("`", "\\textasciigrave{}", $text);
-	
-		// Clean up backslashes from before
-		$text = str_replace("\\\\", "\\textbackslash{}", $text); // Substitute backslashes from first step.
-		$text = str_replace("\n", "\\\\", trim($text)); // Replace newlines (trim is in case of leading \\)
-		return $text;
 	}
 }
 ?>
---
+++ navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo $url;?>index.php">科學計算軟體</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="<?php echo $url;?>index.php">首頁</a></li>
+              <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">作業 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="<?php echo $url;?>hw/hw1.php">作業一</a></li>
+            <li><a href="<?php echo $url;?>hw/hw2.php">作業二</a></li>
+            
+          </ul>
+        </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              
+              <!--<li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>-->
+              <?php
+              if(isset($_COOKIE["token"])) {
+              ?>  
+                  <li><a ><?php echo $_COOKIE["user"];?>，您好！</a></li>
+                  <li><a href="<?php echo $url;?>logout.php">登出</a></li>
+              <?php
+              }else{
+              ?>
+                  <li><a href="<?php echo $url;?>login.php">登入</a></li>
+              
+              <?php
+              }
+              ?>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
