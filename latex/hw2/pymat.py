05c2f1c575baf7e059545de1d950083ead6ec30b
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-09-27 19:24:13 +0800

    initial hw2

--- latex/hw2/pymat.py
+++ latex/hw2/pymat.py
@@ -2,7 +2,8 @@ import sys, os
 from numpy import *
 import scipy.io as sio
 
-test1=random.rand(3,4);
-array=random.rand(3,4);
+test1=random.rand(3,4)
+array=random.rand(3,4)
 filename=sys.argv[1]
-sio.savemat(filename+'.mat', {'test':array,'test2':test1})
+stdid=sys.argv[2]
+sio.savemat(filename+'.mat', {'name':stdid,'test2':test1})
