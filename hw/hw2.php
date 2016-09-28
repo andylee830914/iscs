c4e588c2e53dc6824abed92006d3fa6ab6ab5b1d
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-09-29 01:18:02 +0800

    prepare for hw2

--- hw/hw2.php
+++ hw/hw2.php
@@ -12,7 +12,7 @@ require_once('../latex/MATLABTemplate.php');
 $quy="select * from user where idnumber='".$name."'";
 $result=mysql_query($quy);
 $moodleid=mysql_result($result,0,1);
-$hw=$moodleid % 4;
+$hw=intval(substr($name,5,4)) % 2;
 $test = "";
 if(isset($_GET['t'])) {
 	// Make the LaTeX file and send it through
@@ -23,7 +23,7 @@ if(isset($_GET['t'])) {
 	);
 
 	try {
-		LatexTemplate::download($data, 'latex/hw2/hw2_'.$hw.'.tex', $name.'_HW1.pdf');
+		LatexTemplate::download($data, $url.'latex/hw2/hw2_'.$hw.'.tex', $name.'_HW2.pdf');
     $date       = date('Y-m-d H:i:s');
     $quy2="INSERT INTO download (moodleid,time, download, hw) VALUES ('".$moodleid."','".$date."','".$hw."',  '1')";
     mysql_query($quy2);
@@ -74,16 +74,15 @@ if (isset($_GET['py'])||isset($_GET['t'])) {
         <div class="panel-body">
           <h3>作業要求</h3>
           <ul>
-              <li>請按底下的「下載檔案」，下載本次的作業題目PDF檔</li>                           
+              <li>請按底下的「下載檔案」及「下載資料」，下載本次的 作業題目PDF檔 與 作業資料mat檔</li>                           
               <li>上傳規定：
                   <ul>
                   <li>請回 moodle 上傳本次作業。</li>
-                  <li>"作業題目"、"MuPAD檔"、"螢幕截圖"、"輸出PDF檔"請一起放進資料夾，使用zip壓縮。</li>
-                  <li>請把學號姓名打在MuPAD檔第一行。</li>                                                                                    
-                  <li>檔名：<strong>學號_HW1.zip</strong></li>               
+                  <li>"作業題目"、"MAT檔"、"螢幕截圖"請一起放進資料夾，使用zip壓縮。</li>                                                                                    
+                  <li>檔名：<strong>學號_HW2.zip</strong></li>               
                   </ul>
               </li>
-              <li>期限：2016-09-26 17:00</li>
+              <li>期限：2016-10-06 17:00</li>
               <li>作業請自己寫！不會寫可以跟同學、老師或助教討論！</li>               
 
             
--- latex/hw2/pymat.py
+++ latex/hw2/pymat.py
@@ -1,9 +1,19 @@
 import sys, os
-from numpy import *
+import numpy as np
 import scipy.io as sio
 
-test1=random.rand(3,4)
-array=random.rand(3,4)
+
 filename=sys.argv[1]
 stdid=sys.argv[2]
-sio.savemat(filename+'.mat', {'name':stdid,'test2':test1})
+
+version='v'+str(int(stdid[5:9]) % 2)
+q2a={
+    'v0':np.matrix([[1.0,2,0],[0,3,0],[2,-4,2]]),
+    'v1':np.matrix([[3.0,1,1],[2,4,2],[-1,-1,1]])
+    }
+q2q={
+    'v0':np.matrix([[0,-1.0,-1],[0,0,-1],[1,2,2]]),
+    'v1':np.matrix([[-1.0,0,-1],[-2,-1,0],[1,1,1]])    
+    }
+
+sio.savemat(filename+'.mat', {'name':stdid,'A':q2a[version],'Q':q2q[version]})
