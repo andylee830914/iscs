73437d3ad4026949129b447d713677127d073518
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-11-02 09:57:21 +0800

    fix again

--- upload/upload.php
+++ upload/upload.php
@@ -29,7 +29,7 @@ if (!empty($_FILES)) {
     
 
     if(!file_exists($targetPath)){
-        mkdir($targetPath, 0775);
+        mkdir($targetPath, 0777);
         // chmod($targetPath, 0777);
     }
     if(in_array($_FILES['userfile']['type'],$accept)){
