55e7c590ce7ab0a616c6d1c0725056debcd4934c
Lee,Yu-Hsun <andylee0914.tw@gmail.com>
2016-11-04 10:53:34 +0800

    fix ubuntu latex

--- latex/LatexTemplate.php
+++ latex/LatexTemplate.php
@@ -28,7 +28,7 @@ class LatexTemplate {
 		file_put_contents($tex_f, ob_get_clean());
 	
 		// Run xelatex (Used because of native unicode and TTF font support)
-		$cmd = sprintf("/Library/TeX/texbin/xelatex -interaction nonstopmode -halt-on-error %s",
+		$cmd = sprintf("xelatex -interaction nonstopmode -halt-on-error %s",
 				escapeshellarg($tex_f));
 		chdir(sys_get_temp_dir());
 		exec($cmd, $foo, $ret);
