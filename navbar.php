f01901191b26ca5b384afc79a0743e6790f7c3b5
Lee,Yu-Hsun <andylee0914.tw@gmail.com>
2016-11-10 13:17:51 +0800

    hw9

--- navbar.php
+++ navbar.php
@@ -27,6 +27,7 @@
                     <li><a href="<?php echo $url;?>hw/hw6.php">作業六</a></li>
                     <li><a href="<?php echo $url;?>hw/hw7.php">作業七</a></li>
                     <li><a href="<?php echo $url;?>hw/hw8.php">作業八</a></li>
+                    <li><a href="<?php echo $url;?>hw/hw9.php">作業九</a></li>
                   </ul>
               </li>   
               <?php if($_SERVER['SCRIPT_NAME']=="/iscs/result/visual.php"){?>
