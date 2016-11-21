a362ec6826a3a01c38c698fd92e8f7d15c89a6f5
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-11-21 15:22:04 +0800

    fix code

---
+++ token.php
@@ -0,0 +1,15 @@
+<?php
+        $domainname = "http://moodle.ncku.edu.tw";
+        $where['username'] = $_GET['username'];
+        $where['password'] = $_GET['password'];
+        $where['service']  = 'ncku_moodle_app';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $domainname.'/login/token.php');
+        curl_setopt($ch, CURLOPT_POST, true); // 啟用POST
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($where));
+        $result=curl_exec($ch);
+        curl_close($ch);
+        $result=json_decode($result);
+        print_r($result);
+?>
